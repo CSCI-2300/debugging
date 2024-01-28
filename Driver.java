4f808faecdb2e310e863312ca69154eaf5b86947
Kate Holdener <kate.holdener@slu.edu>
2024-01-28 16:05:25 -0600

    added example of an infinite loop

---
+++ Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+    public static void main(String []args){
+        int counter = 1;
+        while (counter <= 10){
+            System.out.println(1);
+        }
+        counter = counter + 1;
+    }
+}
