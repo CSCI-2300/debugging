6d6b0a3859255014fe26bb2e075dae15ae241e93
Kate Holdener <kate.holdener@slu.edu>
2024-01-28 16:16:01 -0600

    added more debugging examples

---
+++ evenSum/EvenSumCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class EvenSumCalculator {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number, I'll calculate an even sum from 1 to your number");
+        int number = scan.nextInt();
+        int sum = calculateEvenSum(number);
+        System.out.println("Sum of even numbers from 1 to " + number + " is: " + sum);
+    }
+    
+    public static int calculateEvenSum(int n) {
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % 2 == 0) { // bug: should be i % 2 == 0 to check for even numbers
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
